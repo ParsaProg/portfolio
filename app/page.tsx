6ea174c582e50787b67bf3729a312b9bc02f6049
ParsaProg <parsashaabani3@gmail.com>
2025-03-19 15:44:51 +0330

    create header i made for you text and bolds in home

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,11 @@ import useTitle from "./hooks/useTitle";
 
 export default function Home() {
   useTitle("Home | ParsaShaabani");
-  return <div className=""></div>;
+  return (
+    <div className="mt-[80px] m-auto text-white">
+      <h1 className="font-bold text-[3rem] text-center">I made this for you</h1>
+      <h2 className="font-[500] text-xl text-center text-[#A1A1AA]">Featuring current work and studies in a SvelteKit-based portfolio.</h2>
+      <p className="font-[500] text-sm text-center text-[#4ade80] mt-3">Inspired by others, I share my open-source derived work with the community.</p>
+    </div>
+  );
 }
