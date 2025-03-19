39ecc986a76f3f1f17472737904c2b979b164f97
ParsaProg <parsashaabani3@gmail.com>
2025-03-19 17:56:23 +0330

    add main footer component to site ui

---
+++ components/site/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return <div className="mt-[50px]">
+        <p className="text-[#97a0a5] font-[500] text-md text-center mb-5">© 2025 — Parsa Shaabani. All rights reserved.</p>
+    </div>
+}
