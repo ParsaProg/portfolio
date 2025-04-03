76f43b0f2a9b6d6e530671b8c8d15a9def8d4614
ParsaProg <parsashaabani3@gmail.com>
2025-04-03 23:52:02 +0330

    favicon change for better seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
         <title>پارسا شعبانی | Parsa Shaabani - تخصص در برنامه‌نویسی</title>
         <meta
           name="description"
