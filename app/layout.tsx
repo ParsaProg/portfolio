622e7fcf81fabeaf48315e27f5c6720eb6a3caf0
ParsaProg <parsashaabani3@gmail.com>
2025-04-04 14:43:57 +0330

    image seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,21 +30,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href={`${favicon.src}/?v=2`} type="image/x-icon" />
+        <link rel="icon" href={favicon.src} sizes="any" />
+        <link rel="icon" href={favicon.src} type="image/svg+xml" />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href={favicon.src}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href={favicon.src}
         />
         ّ
         <meta
